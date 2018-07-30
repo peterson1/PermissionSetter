e38ea719b07227dd01bd2aabf2d4b055281d674d
Peterson Salamat <petersonsalamat@gmail.com>
2018-07-30 12:21:11 +0800

    Programmatic NLog config instead of file-based

--- FolderWatcherExe/App.xaml.cs
+++ FolderWatcherExe/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using CommonTools.Lib45.ApplicationTools;
 using FolderWatcherExe.Configuration;
-using PermissionSetter.Lib11.Configuration;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 using System.Windows;
 
 namespace FolderWatcherExe
@@ -11,8 +13,23 @@ namespace FolderWatcherExe
         {
             base.OnStartup(e);
 
+            LogManager.Configuration = CreateNLogConfig();
+
             this.Initialize<CommandLineArgs>(args
                 => new MainWindowVM(args).Show<MainWindow>());
+        }
+
+
+        private LoggingConfiguration CreateNLogConfig()
+        {
+            var cfg = new LoggingConfiguration();
+            var trg = new FileTarget()
+            {
+                FileName = "${basedir}/logs/${shortdate}.log",
+                Layout   = "[${time}]   ${message}  ${exception}",
+            };
+            cfg.AddRule(LogLevel.Trace, LogLevel.Fatal, trg);
+            return cfg;
         }
     }
 }
